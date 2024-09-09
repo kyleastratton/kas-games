774ab81df0a76085667507ba2e5c0ea687c9fd52
Kyle Stratton <kyle.stratton@icloud.com>
2024-09-09 18:30:02 +0100

    add dice roll

---
+++ dice-roll/script.js
@@ -0,0 +1,21 @@
+var elDiceOne = document.getElementById('dice1');
+var elDiceTwo = document.getElementById('dice2');
+
+function rollDice() {
+  var diceOne   = Math.floor((Math.random() * 6) + 1);
+  var diceTwo   = Math.floor((Math.random() * 6) + 1);
+  for (var i = 1; i <= 6; i++) {
+    elDiceOne.classList.remove('show-' + i);
+    if (diceOne === i) {
+      elDiceOne.classList.add('show-' + i);
+    }
+  }
+
+  for (var k = 1; k <= 6; k++) {
+    elDiceTwo.classList.remove('show-' + k);
+    if (diceTwo === k) {
+      elDiceTwo.classList.add('show-' + k);
+    }
+  } 
+  setTimeout(rollDice(), 1000);
+}
